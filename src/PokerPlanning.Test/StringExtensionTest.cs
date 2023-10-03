8308a428c2ca5ca2543da9cddfa0ce7f6772b6fc
Faruk Abdo Feres <faruk_feres@hotmail.com>
2023-10-02 21:00:04 -0300

    [test] added unitary test to created extension

---
+++ src/PokerPlanning.Test/StringExtensionTest.cs
@@ -0,0 +1,22 @@
+﻿using static PokerPlanning.Extensions.StringExtension;
+
+namespace PokerPlanning.Test
+{
+    public class StringExtensionTest
+    {
+        [Theory]
+        [InlineData("1", "1")]
+        [InlineData("1a", "1")]
+        [InlineData("a1", "1")]
+        [InlineData("", "")]
+        [InlineData("☕", "")]
+        public void Test1(string input, string result)
+        {
+            // Arrange
+            // Act
+            var output = input.ToDigitsOnly();
+            // Assert
+            Assert.Equal(result, output);
+        }
+    }
+}
