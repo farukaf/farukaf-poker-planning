e063f5ba99d47950bb3a5cac95687d05d84556ac
Faruk Abdo Feres <faruk_feres@hotmail.com>
2024-01-12 21:32:46 -0300

    [feat] created janitor service

---
+++ src/PokerPlanning/BackgroundServices/JanitorService.cs
@@ -0,0 +1,21 @@
+﻿
+using PokerPlanning.Services;
+
+namespace PokerPlanning.BackgroundServices
+{
+    public sealed class JanitorService : BackgroundService
+    {
+        private readonly IRoomService _roomService;
+        public JanitorService(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            return Task.CompletedTask;
+        }
+    }
+}
