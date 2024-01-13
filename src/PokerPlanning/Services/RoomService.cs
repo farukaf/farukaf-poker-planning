5e13e26cb7105f7d24e9eb9ab8ee23182e6a3f60
Faruk Abdo Feres <faruk_feres@hotmail.com>
2024-01-12 23:52:21 -0300

    [chore] added log when a new room is created. changed variable name

--- src/PokerPlanning/Services/RoomService.cs
+++ src/PokerPlanning/Services/RoomService.cs
@@ -5,22 +5,29 @@ namespace PokerPlanning.Services;
 
 public sealed class RoomService : IRoomService
 {
-    public RoomService()
+    private readonly ILogger<RoomService> _logger;
+    public RoomService(ILogger<RoomService> logger)
     {
         Rooms = new ConcurrentDictionary<Guid, Room>();
+        _logger = logger;
     }
 
     public ConcurrentDictionary<Guid, Room> Rooms { get; set; }
 
+    /// <summary>
+    /// Clean the rooms
+    /// </summary>
+    /// <param name="olderThan"></param>
+    /// <returns>Number of rooms was closed</returns>
     public Task<int> CleanRooms(DateTime olderThan)
     {
-        var count = 0;
+        var closedRooms = 0;
         var dirtyRooms = Rooms.Where(x => x.Value.UpdateAt < olderThan);
         foreach (var dirtyRoom in dirtyRooms)
             if (Rooms.TryRemove(dirtyRoom.Key, out _))
-                count++;
+                closedRooms++;
 
-        return Task.FromResult(count);
+        return Task.FromResult(closedRooms);
     }
 
     public Guid Create(string[] cardValues)
@@ -30,6 +37,8 @@ public sealed class RoomService : IRoomService
             CardValues = cardValues
         };
         Rooms.TryAdd(room.Id, room);
+
+        _logger.LogInformation($"Room {room.Id} created.");
 
         return room.Id;
     }
